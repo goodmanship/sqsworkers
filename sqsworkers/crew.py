6a89d7d3a089f5f28f1821d6a1cd07a0178dd6ca
Rio Goodman <rgoodman@atlassian.com>
2017-10-02 17:56:38 -0700

    Allow for sqs resource to be specified directly

--- sqsworkers/crew.py
+++ sqsworkers/crew.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import raven
+import re
 from threading import Thread, currentThread
 import time
 import traceback
@@ -34,8 +35,9 @@ class DummyStatsd():
 
 class Crew():
     # options = {
-    #  *  'sqs_session': None,
-    #  *  'queue_name': 'name',
+    #  @  'sqs_session': None,
+    #  @  'queue_name': 'name',
+    #  @  'sqs_resource': 'resource',
     #  *  'MessageProcessor': MsgProcessor,
     #  *  'logger': logging.getLogger('default'),
     #     'statsd': Dummy,
@@ -43,19 +45,31 @@ class Crew():
     #     'worker_limit': 10
     # }
     # * = required
+    # @ = required: (session + name) or (url)
 
     def __init__(self, **kwargs):
-        self.crew_name = 'crew-%s-%s-%s' % (os.getpid(), kwargs['queue_name'], str(time.time()))
         self.workers = []
         self.supervisor = None
-
-        self.sqs_session = kwargs['sqs_session']
-        self.queue_name = kwargs['queue_name']
+        self.sqs_session = kwargs['sqs_session'] if 'sqs_session' in kwargs else None
+        self.queue_name = kwargs['queue_name'] if 'queue_name' in kwargs else None
+        self.sqs_resource = kwargs['sqs_resource'] if 'sqs_resource' in kwargs else None
         self.MessageProcessor = kwargs['MessageProcessor']
-        self.logger = logging.LoggerAdapter(kwargs['logger'], extra={'extra': {'crew_name': self.crew_name}})
+        self.name = self.make_name(self.queue_name, self.sqs_resource)
+        self.logger = logging.LoggerAdapter(kwargs['logger'], extra={'extra': {'crew_name': self.name}})
         self.statsd = kwargs['statsd'] if 'statsd' in kwargs else DummyStatsd(self.logger)
         self.sentry = kwargs['sentry'] if 'sentry' in kwargs else None
         self.worker_limit = kwargs['worker_limit'] if 'worker_limit' in kwargs else 10
+        if not ((self.sqs_session and self.queue_name) or self.sqs_resource):
+            raise TypeError('Required arguments not provided.  Either provide (sqs_session + queue_name) or sqs_resource.')
+
+    def make_name(self, name, url):
+        try:
+            if name:
+                return 'crew-%s-%s-%s' % (os.getpid(), name, str(time.time()))
+            else:
+                return 'crew-%s-%s-%s' % (os.getpid(), re.split('http:\/\/|\.',url)[-2], str(time.time()))
+        except:
+            return 'crew-%s-%s-%s' % (os.getpid(), 'noname', str(time.time()))
 
     def start(self):
         try:
@@ -96,12 +110,13 @@ class Worker(CrewMember):
     def __init__(self, crew):
         self.crew = crew
         self.sqs_session = self.crew.sqs_session
+        self.sqs_resource = self.crew.sqs_resource
         self._real_run = self.run
         self.run = self._wrap_run
-        self.worker_name = 'worker-%s-%s-%s' % (os.getpid(), currentThread().getName(), str(time.time()))
+        self.name = 'worker-%s-%s-%s' % (os.getpid(), currentThread().getName(), str(time.time()))
         self.queue_name = self.crew.queue_name
-        self.crew.logger.info('new worker starting with name: %s' % (self.worker_name))
-        self.logger = logging.LoggerAdapter(self.crew.logger, extra={'extra': {'worker_name': self.worker_name, 'crew_name': self.crew.crew_name}})
+        self.crew.logger.info('new worker starting with name: %s' % (self.name))
+        self.logger = logging.LoggerAdapter(self.crew.logger, extra={'extra': {'worker_name': self.name, 'crew_name': self.crew.name}})
         self.logger = self.crew.logger
         CrewMember.__init__(self)
 
@@ -112,13 +127,15 @@ class Worker(CrewMember):
             raise
         except Exception as e:
             self.crew.sentry.captureException() if 'sentry' in self.crew else None
-            log_uncaught_exception(e, logger=self.logger, context={'worker_name': self.worker_name, 'crew_name': self.crew.crew_name})
+            log_uncaught_exception(e, logger=self.logger, context={'worker_name': self.name, 'crew_name': self.crew.name})
 
     def run(self):
-        self.logger.info('thread %s starting now' % self.worker_name)
-
-        sqs_connection = self.sqs_session.resource('sqs', region_name=self.sqs_session.region_name)
-        self.queue = sqs_connection.get_queue_by_name(QueueName=self.queue_name)
+        self.logger.info('thread %s starting now' % self.name)
+        if self.sqs_resource == None:
+            sqs_connection = self.sqs_session.resource('sqs', region_name=self.sqs_session.region_name)
+            self.queue = sqs_connection.get_queue_by_name(QueueName=self.queue_name)
+        else:
+            self.sqs_resource = self.crew.sqs_resource
 
         self.logger.info(
             'connected to sqs, approx. %s msgs on queue' %
@@ -155,7 +172,7 @@ class Supervisor(CrewMember):
     def __init__(self, crew):
         self._real_run = self.run
         self.run = self._wrap_run
-        self.supervisor_name = 'supervisor-%s-%s' % (os.getpid(), str(time.time()))
+        self.name = 'supervisor-%s-%s' % (os.getpid(), str(time.time()))
         self.crew = crew
         CrewMember.__init__(self)
 
@@ -166,7 +183,7 @@ class Supervisor(CrewMember):
             raise
         except Exception as e:
             self.crew.sentry.captureException() if 'sentry' in self.crew else None
-            log_uncaught_exception(e, logger=self.crew.logger, context={'supervisor_name': self.supervisor_name, 'crew_name': self.crew.crew_name})
+            log_uncaught_exception(e, logger=self.crew.logger, context={'supervisor_name': self.name, 'crew_name': self.crew.name})
             self._wrap_run()
 
     def run(self):
@@ -176,19 +193,19 @@ class Supervisor(CrewMember):
 
     def supervise(self):
         good_workers = []
-        self.crew.logger.info('supervising %s worker on workers: %s' % (self.supervisor_name, self.crew.workers))
+        self.crew.logger.info('supervising %s worker on workers: %s' % (self.name, self.crew.workers))
         for worker in self.crew.workers:
             if worker.is_alive():
-                self.crew.logger.info('worker %s is alive' % (worker.worker_name))
+                self.crew.logger.info('worker %s is alive' % (worker.name))
                 good_workers.append(worker)
             else:
-                self.crew.logger.warn('worker %s is dead, hiring a new one...' % (worker.worker_name))
-                self.crew.statsd.increment('workers.dead', 1, tags=[self.crew.crew_name])
+                self.crew.logger.warn('worker %s is dead, hiring a new one...' % (worker.name))
+                self.crew.statsd.increment('workers.dead', 1, tags=[self.crew.name])
                 new_worker = Worker(self.crew)
-                self.crew.logger.info('hired a new worker %s, staring it up...' % (new_worker.worker_name))
+                self.crew.logger.info('hired a new worker %s, staring it up...' % (new_worker.name))
                 new_worker.start()
-                self.crew.logger.info('started a new worker %s' % (new_worker.worker_name))
+                self.crew.logger.info('started a new worker %s' % (new_worker.name))
                 good_workers.append(new_worker)
         self.crew.logger.info('total number of workers after supervision: %s' % (str(len(good_workers))))
-        self.crew.statsd.gauge('workers.employed', len(good_workers), tags=[self.crew.crew_name])
+        self.crew.statsd.gauge('workers.employed', len(good_workers), tags=[self.crew.name])
         self.crew.workers = good_workers
