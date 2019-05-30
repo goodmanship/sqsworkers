c58e4c2832ee653400636151be5c1531bb8d973c
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2019-05-30 12:59:56 -0700

    move cls

--- sqsworkers/base.py
+++ sqsworkers/base.py
@@ -16,19 +16,6 @@ from sqsworkers import MessageMetadata
 from sqsworkers import interfaces
 
 
-class StatsDBase(interfaces.StatsDInterface):
-    """Implements the base statsd interface."""
-
-    def __init__(self, logger=None):
-        self.logger = logging.getLogger() if logger is None else logger
-
-    def increment(self, *args, **kwargs):
-        self.logger.info(f"statsd increment invoked: {args}, {kwargs}")
-
-    def gauge(self, *args, **kwargs):
-        self.logger.info(f"statsd gauge invoked: {args}, {kwargs}")
-
-
 class BaseListener(interfaces.CrewInterface):
     """
     This class polls on an sqs queue, delegating the work to a message processor that runs in a threadpool.
@@ -259,3 +246,16 @@ class BaseListener(interfaces.CrewInterface):
                     }
                 ]
             )
+
+
+class StatsDBase(interfaces.StatsDInterface):
+    """Implements the base statsd interface."""
+
+    def __init__(self, logger=None):
+        self.logger = logging.getLogger() if logger is None else logger
+
+    def increment(self, *args, **kwargs):
+        self.logger.info(f"statsd increment invoked: {args}, {kwargs}")
+
+    def gauge(self, *args, **kwargs):
+        self.logger.info(f"statsd gauge invoked: {args}, {kwargs}")
